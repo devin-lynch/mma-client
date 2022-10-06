f12c2f39c1b0f2af1dca7354a7698c64f4160fba
devin-lynch <devin.l.lynch@gmail.com>
2022-10-05 21:08:24 -0500

    Show all page working

--- src/components/routes/MartialArtists.jsx
+++ src/components/routes/MartialArtists.jsx
@@ -3,9 +3,44 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 export default function MartialArtists() {
+    // martial artists from the backend
+    const [martialArtist, setMartialArtist] = useState([])
+    // state for messages from the backend
+    const [errorMessage, setErrorMessage] = useState('')
+    
+    console.log('server url', process.env.REACT_APP_SERVER_URL)
+    useEffect(() => {
+        const getMartialArtists = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/martialartist`)
+                // console.log(response.data)
+                // TODO: sort by date and only show the most recent bounties
+                setMartialArtist(response.data)
+            } catch(err) {
+                console.warn(err)
+                if (err.response) {
+                    setErrorMessage(err.response.data.message)
+                }
+            }
+        }
+        getMartialArtists()
+    }, [])
+    
+    const martialArtistLinks = martialArtist.map(artist => {
+        return (
+            <div key={artist._id}>
+                <Link to={`/martialartists/${artist._id}`}>
+                    {artist.name}
+                </Link>
+            </div>
+        )
+    })
+
     return (
         <div>
-            Martial Artists
+            <h1>All Martial Artists</h1>
+
+            {martialArtistLinks}
         </div>
     )
 }
