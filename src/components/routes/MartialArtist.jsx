2fe80e2abd1b04f709e96663488acbd6eb1111cd
devin-lynch <devin.l.lynch@gmail.com>
2022-10-05 21:29:03 -0500

    Complete detail route with destroy operation

--- src/components/routes/MartialArtist.jsx
+++ src/components/routes/MartialArtist.jsx
@@ -3,16 +3,60 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function MartialArtist() {
-    const [bounty, setBounty] = useState({})
+    const [martialArtist, setMartialArtist] = useState({})
     const [errorMessage, setErrorMessage] = useState('')
     
     const { id } = useParams()
     const navigate = useNavigate()
 
-    
+    useEffect(() => {
+        const getMartialArtist = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/martialartist/${id}`)
+                // console.log(response.data)
+                setMartialArtist(response.data)
+            } catch(err) {
+                if (err.response) {
+                    setErrorMessage(err.response.data.message)
+                }                
+            }
+        }
+        getMartialArtist()
+    }, [])
+
+    const handleDelete = async () => {
+        try {
+            // axios to the backend to delete this martial artists
+            await axios.delete(`${process.env.REACT_APP_SERVER_URL}/martialartist/${id}`)
+            // after deletion, navigate back to /martialartists
+            navigate('/martialartists')
+        } catch(err) {
+            console.warn(err)
+            if (err.response) {
+                setErrorMessage(err.response.data.message)            
+            }            
+        }
+    }
+
     return (
         <div>
-            Martial Artist
+            <h1>Martial Artist Details</h1>
+
+            <p>{errorMessage}</p>
+
+            <div>
+                <Link to={`/martialartist/${id}/edit`}><button>Edit</button></Link>
+
+                <button onClick={handleDelete}>Delete</button>
+            </div>
+
+            <div>
+                <h2>{martialArtist.name}</h2>
+
+                <p>{martialArtist.nickname}</p>
+
+                <p>Weightclass: {martialArtist.weightClass}</p>
+            </div>
         </div>
     )
 }
